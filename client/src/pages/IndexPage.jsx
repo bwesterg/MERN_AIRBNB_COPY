c91ad516e26e70fdefab5efbc73c146457aec2ae
bwesterg <benton.westergaard@gmail.com>
2024-01-08 15:26:08 -0700

    rebuilds MERN AIRBNB copy, installing express, mongoose, jwt, axios, bcrypt, cors, dotenv, and sets up login and register routes alongside redirect to placeholder home page.

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,12 @@
+// import { Link } from "react-router-dom";
+// import LoginPage from "./LoginPage";
+import Header from "../Header";
+
+export default function IndexPage() {
+    return (
+        <div>
+            index page here
+        </div>
+    )
+
+}
