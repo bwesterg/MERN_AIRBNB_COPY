a03b57336c258b0171c8a815a005a50e4be68070
bwesterg <benton.westergaard@gmail.com>
2024-01-08 17:37:02 -0700

    persists login and adds header recognition once logged in.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 
@@ -11,7 +12,7 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'lkajsd77hdkjsd3d';
 
 app.use(express.json())
-
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173', 
@@ -48,9 +49,13 @@ app.post('/login', async (req, res) => {
     if (userDoc) {
         const passOk = bcrypt.compareSync(password, userDoc.password)
         if (passOk) {
-            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err,token) => {
-                if (err) throw err;
-                res.cookie('token', token).json('pass ok');
+            jwt.sign({
+                    email:userDoc.email, 
+                    id:userDoc._id, 
+                    // name:userDoc.name
+                }, jwtSecret, {}, (err,token) => {
+                    if (err) throw err;
+                    res.cookie('token', token).json(userDoc);
             });
         } else {
             res.status(422).json('pass not ok');
@@ -58,6 +63,20 @@ app.post('/login', async (req, res) => {
     } else {
         res.json('not found');
     }
+})
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            const {name,email,_id} = await User.findById(userData.id);
+            res.json({name,email,_id});
+        });
+    } else {
+        res.json(null);
+    }
+    // res.json({token});
 })
 
 app.listen(4000);
