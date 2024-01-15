921ac960963a5308e9593c1c13eb4029ac547b29
bwesterg <benton.westergaard@gmail.com>
2024-01-15 15:05:27 -0700

    removes disableReactDevTools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.MONGO_URL === 'production') disableReactDevTools();
+// if (process.env.MONGO_URL === 'production') disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
